e3b6ce1d3eca0f92eacd83e95c30fcaf04530c2b
Rishi Verma <rishioffic@gmail.com>
2025-05-10 23:07:53 +0000

    Updated keys

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import torch
 from pytorch_lightning.loggers import TensorBoardLogger
 import pytorch_lightning as L
 from torchmetrics import MeanSquaredError
-from modules import Mars, MarsDataset
+from modules import Mars, MarsDataset, GeneSetAdaptor
 from torch.utils.data import DataLoader
 from utils import _init_weights
 
@@ -13,8 +13,8 @@ parser.add_argument('--val-data', type=str, default='data/val.h5', help='path to
 parser.add_argument('--batch-size', type=int, default=32, help='batch size for training')
 parser.add_argument('--num-workers', type=int, default=4, help='number of workers for data loading')
 parser.add_argument('--max-epochs', type=int, default=100, help='number of epochs to train')
-parser.add_argument('--adaptor', type=int, default=100, help='Adaptor type')
-parser.add_argument('--criterion', type=int, default=100, help='Criterion type')
+parser.add_argument('--adaptor', type=int, default='gene_set', help='Adaptor type')
+parser.add_argument('--criterion', type=int, default='mse', help='Criterion type')
 args = parser.parse_args()
 
 # Load Datasets
@@ -22,11 +22,13 @@ train_dataset = MarsDataset(args.train_data)
 val_dataset = MarsDataset(args.val_data)
 
 # Load DataLoaders
-train_datalaoder = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
 # Load Adaptor
-# ADAPTOR_DICT = {}
+ADAPTOR_DICT = {
+    'gene_set': GeneSetAdaptor(),
+}
 
 adaptor = ADAPTOR_DICT[args.adaptor]
 adaptor.apply_weights(_init_weights)
@@ -61,4 +63,4 @@ trainer = L.Trainer(
     progress_bar_refresh_rate=20,
 )
 model = Mars()
-trainer.fit(model, train_loader, val_loader)
+trainer.fit(model, train_dataloader, val_dataloader)
