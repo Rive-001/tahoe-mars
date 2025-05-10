c6c2760c84c5f17c085a273c7edae48361e82a72
Rishi Verma <rishioffic@gmail.com>
2025-05-10 20:34:49 +0000

    Change concat init params

--- modules/concat.py
+++ modules/concat.py
@@ -3,25 +3,24 @@ import torch
 
 class ConcatEmbeddings(nn.Module):
 
-    def __init__(self, embedding_list):
+    def __init__(self, embedding_dim, output_dim=512):
 
         super().__init__()
 
-
-        self.embedding_list = embedding_list
-        self.embedding_dim = sum([len(embedding) for embedding in self.embedding_list])
+        self.embedding_dim = embedding_dim
+        self.output_dim = output_dim
 
         self.projection = nn.Sequential(
             nn.Linear(self.embedding_dim, 1024),
             nn.GELU(),
-            nn.Linear(1024, 512),
+            nn.Linear(1024, self.output_dim),
             nn.GELU(),
         )
 
-    def forward(self, x):
+    def forward(self, embedding_list):
 
         # Concatenate the embeddings
-        x = [embedding(x) for embedding in self.embedding_list]
+        x = [embedding(x) for embedding in embedding_list]
         x = torch.cat(x, dim=1)
 
         # Project to a lower dimension
